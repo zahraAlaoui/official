499c8b8aed4a05104b39b3268db989b89bb9d891
zahraAlaoui <sinnersjungle@gmail.com>
2024-05-07 09:36:06 +0200

    cuartocom5

--- scripts.js
+++ scripts.js
@@ -58,7 +58,7 @@ function enviarCorreo() {
     var email = document.getElementById('email').value;
     var mensaje = document.getElementById('mensaje').value;
     
-    var mailto_link = 'mailto:javilansanch@gmail.com?subject=' + encodeURIComponent(asunto) + '&body=' + encodeURIComponent(mensaje);
+    var mailto_link = 'mailto:javilansanch@gmail.com?subject=' + encodeURIComponent(asunto) + '&body=' + encodeURIComponent(mensaje + "\n\nCorreo electrónico: " + email);
     
     window.location.href = mailto_link;
 }
